5bb6048dfadb5ad1f69f693f95d8b16abd9843b8
Susana Marquina <smarq86@gmail.com>
2021-12-27 18:47:59 -0800

    roll button added

---
+++ index.js
@@ -0,0 +1,28 @@
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
+
+const player1Dice = document.getElementById('player1Dice');
+const player2Dice = document.getElementById('player2Dice');
+const player1Scoreboard = document.getElementById('player1Scoreboard');
+const player2Scoreboard = document.getElementById('player2Scoreboard');
+const rollBtn = document.getElementById('rollBtn');
+const resetBtn = document.getElementById('resetBtn');
+const message = document.getElementById('message');
+
+rollBtn.addEventListener('click', function () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+
+  if (player1Turn) {
+    player1Dice.textContent = randomNumber;
+    player1Dice.classList.remove('active');
+    player2Dice.classList.add('active');
+    message.textContent = 'Player 2 Turn';
+  } else {
+    player2Dice.textContent = randomNumber;
+    player2Dice.classList.remove('active');
+    player1Dice.classList.add('active');
+    message.textContent = 'Player 1 Turn';
+  }
+  player1Turn = !player1Turn;
+});
